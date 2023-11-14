35debb41a7f97ebcfd50b1f07e55d32a855ec97a
BMortis <bayron.p@outlook.cl>
2023-11-14 20:53:34 -0300

    Agregados metodos al js, calcularPuntaje, esRespuestaValida, bloqueo de inputs

--- script.js
+++ script.js
@@ -8,16 +8,51 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-button.addEventListener('click', (e) => {
-    fetch('/stop-game', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ player: e.target.dataset.player }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Respuesta del servidor:', data);
+function handleStopButtonClick() {
+    // Deshabilitar todos los campos de entrada en la página
+    const allInputs = document.querySelectorAll('input[type="text"]');
+    allInputs.forEach(input => {
+        input.disabled = true;
     });
-});
+
+    //calcular puntajes, etc.
+    for (let i = 1; i <= 7; i++) { // Asumiendo 7 jugadores
+        let puntajeJugador = calcularPuntaje(i);
+        console.log(`Puntaje del Jugador ${i}: ${puntajeJugador}`);
+        // Aquí puedes actualizar el puntaje en la interfaz del juego
+    }
+    
+}
+
+function calcularPuntaje(jugador) {
+    let puntaje = 0;
+    // Suponiendo que cada respuesta correcta vale 10 puntos, por ejemplo
+
+    // Seleccionar todos los inputs del jugador
+    const inputsJugador = document.querySelectorAll(`.input-jugador-${jugador}`);
+
+    inputsJugador.forEach(input => {
+        if (input.value !== "" && esRespuestaValida(input.value)) {
+            puntaje += 100;
+        }
+    });
+
+    // Aquí puedes agregar otras reglas de puntuación si las hay
+
+    return puntaje;
+}
+
+function esRespuestaValida(respuesta, letraJuego) {
+    // Eliminar espacios al inicio y al final de la respuesta
+    respuesta = respuesta.trim();
+
+    // Verificar que la respuesta no esté vacía
+    if (respuesta === "") {
+        return false;
+    }
+
+    // Comparar la primera letra de la respuesta con la letra del juego
+    // Convertir ambas a minúsculas (o mayúsculas) para la comparación
+    return respuesta.toLowerCase().startsWith(letraJuego.toLowerCase());
+}
+
